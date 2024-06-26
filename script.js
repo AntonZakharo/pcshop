f11887f560dd20b2ef25e61cd959ef4162776d92
AntonZakharo <antonchik2019top@gmail.com>
2024-06-26 20:48:04 +0300

    commit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-ssylky = document.querySelectorAll('.menu-element')
+
+function caption_pc_func(caption_pc) {
+    caption_pc_total = ''
+        for (i in caption_pc) {
+            caption_pc_total += i + '<br>'
+            return caption_pc_total
+        }
+}
+
+
+
+
+
+
+let ssylky = document.querySelectorAll('.menu-element')
 ssylky.forEach(element => {
     element.addEventListener('click', function(e) {
         e.preventDefault()
@@ -7,14 +21,44 @@ ssylky.forEach(element => {
         })
     })
 });
-pc_cards = document.querySelectorAll('.card-pc')
+let pc_cards = document.querySelectorAll('.card-pc')
 pc_cards.forEach(element => {
     element.addEventListener('click', function(e) {
         e.preventDefault()
-        dark = document.querySelector('#dark')
+
+        let image_pc = element.querySelector('.pc-image').src
+        let name_pc = element.querySelector('.name-pc').innerHTML
+        let caption_pc = element.querySelector('.caption').innerHTML
+        let price = element.querySelector('.price').innerHTML
+        let name_pc_card = document.querySelector('.name-pc-card')
+        let caption_card = document.querySelector('.caption-card')
+        let image_card = document.querySelector('.image-card')
+        name_pc_card.innerHTML = name_pc
+        caption_card.innerHTML = `Характеристики:<br> ${caption_pc}<br><br>Стоимость: <p class="price"> ${price} </p>`
+        image_card.src = image_pc
+        
+        let dark = document.querySelector('#dark')
         dark.style.display = 'flex'
-        body = document.querySelector('body')
+        let body = document.querySelector('body')
         body.style.overflow = 'hidden'
     }
     )
 })
+let exit = document.querySelector('.exit-card')
+exit.addEventListener('click', function() {
+    anime({
+        targets: '.exit-card',
+        keyframes: [{background: '#858585', duration: 150},
+                    {background: '#ffffff', duration: 0}],
+        easing: 'linear'
+    }) 
+    setTimeout(function() {
+        let dark = document.querySelector('#dark')
+        dark.style.display = 'none'
+        let body = document.querySelector('body')
+        body.style.overflow = 'visible'
+    }, 200)
+})
+
+
+
